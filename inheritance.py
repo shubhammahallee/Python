7b9764258105cabbeacd7d4f27b73c251893a896
Shubham Mahalle <shubhammahallee@gmail.com>
2026-03-03 18:26:28 +0530

    Add files via upload

---
+++ inheritance.py
@@ -0,0 +1,23 @@
+class Employee:
+    start_time = "10am"
+    end_time = "6pm"
+    
+    def change_time(self, new_end_time):
+        self.end_time = new_end_time
+
+
+class Teacher(Employee):
+    def __init__(self, subject):
+        self.subject = subject
+        
+class AdminStaff(Employee):
+    def __init__(self,role):
+        self.role = role
+
+
+t1 = Teacher("Science")
+
+t1.change_time("5pm")
+staff1 = AdminStaff("HOD")
+
+print(t1.subject, t1.start_time, t1.end_time,staff1.role) 
