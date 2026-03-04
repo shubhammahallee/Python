2639e580488565313fba09f27075c2b77988d0af
Shubham Mahalle <shubhammahallee@gmail.com>
2026-03-04 17:20:50 +0530

    Add files via upload

---
+++ multilevel_Inheritance.py
@@ -0,0 +1,18 @@
+class Employee:
+    start_time = "10am"
+    end_time = "6pm"
+    
+
+class AdminStaff(Employee):
+    def __init__(self, role):    
+        self.role = role
+        
+
+class Accountant(AdminStaff):
+    def __init__(self, salary, role):
+        super().__init__(role)  
+        self.salary = salary
+        
+
+acc1 = Accountant(97_500, "clerk")
+print(acc1.role, acc1.salary)
