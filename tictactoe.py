3289fa1a296bd476f0b08ca394ea06ed7f56308b
Shubham Mahalle <shubhammahallee@gmail.com>
2026-07-29 08:05:52 +0530

    Update tictactoe.py

--- tictactoe.py
+++ tictactoe.py
@@ -1,7 +1,7 @@
 board = [" "]*9
 
 def print_board():
-   print(f"| {board[0]} | {board[1]} | {board[2]} |")
+   print(f"| {board[0]} | {board[1]} | {board[2]} |") 
    print(f"| {board[3]} | {board[4]} | {board[5]} |")
    print(f"| {board[6]} | {board[7]} | {board[8]} |")
 
