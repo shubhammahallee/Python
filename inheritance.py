aa0e9236d724f612ea6a91553923ad219391d072
Shubham Mahalle <shubhammahallee@gmail.com>
2026-08-11 09:38:05 +0530

    Update inheritance.py

--- inheritance.py
+++ inheritance.py
@@ -20,4 +20,4 @@ t1 = Teacher("Science")
 t1.change_time("5pm")
 staff1 = AdminStaff("HOD")
 
-print(t1.subject, t1.start_time, t1.end_time,staff1.role) 
+print(t1.subject, t1.start_time, t1.end_time,staff1.role)  
